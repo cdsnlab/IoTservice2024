1f49e048ab20909426975da436a675036837ba62
Hyunju Kim <94519276+iplay93@users.noreply.github.com>
2024-10-14 01:19:26 +0900

    Update vis.py

--- CLAN/utils/vis.py
+++ CLAN/utils/vis.py
@@ -44,7 +44,47 @@ def visualization_roc(class_num, y_onehot_test, y_score, vis_title, vis_path, po
     plt.legend()
     plt.show()
     plt.savefig(vis_path)
+    
+def box_plot_vis_rv(labels, scores, path):
+    # Generate data
+    known_data = [b_elem for a_elem, b_elem in zip(labels, scores) if a_elem == 1] 
+    new_data = [b_elem for a_elem, b_elem in zip(labels, scores) if a_elem == 0]
 
+    # Set the figure size
+    plt.figure(figsize=(4,3))
+    
+    # Create a DataFrame for boxplot
+    data = pd.DataFrame({"Known": pd.Series(known_data).reindex_like(pd.Series(new_data)), "New": new_data})
+
+    # Plot individual data points for 'Known'
+    x = np.random.normal(1, 0.1, len(known_data))
+    plt.plot(x, known_data, mfc='red', mec='k', ms=7, marker="o", linestyle="None")
+
+    # Plot individual data points for 'New'
+    x = np.random.normal(2, 0.1, len(new_data))
+    plt.plot(x, new_data, mfc='black', mec='grey', ms=7, marker="o", linestyle="None")
+
+    # Create boxplot
+    boxplot_dict = data.boxplot(return_type='dict')
+
+    # Set linewidth for each boxplot element
+    linewidth = 2
+    for line in boxplot_dict['boxes']:
+        line.set_linewidth(linewidth)
+    for whisker in boxplot_dict['whiskers']:
+        whisker.set_linewidth(linewidth)
+    for cap in boxplot_dict['caps']:
+        cap.set_linewidth(linewidth)
+    for median in boxplot_dict['medians']:
+        median.set_linewidth(linewidth)
+    for flier in boxplot_dict['fliers']:
+        flier.set_marker('+')
+        flier.set_markersize(8)
+        flier.set_markeredgewidth(linewidth)
+
+    plt.show()
+    plt.savefig(path)
+    
 def print_rs(final_1, final_2, final_3, final_4, final_5, save_path):
      # for extrating results to an excel file
     final_rs =[]
