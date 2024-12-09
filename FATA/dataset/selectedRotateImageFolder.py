3d44c84cec11c404415876b6103d0b48d1968791
chengjinghan <chengjh_cqut@163.com>
2024-12-09 14:34:01 +0900

    add prepare test data

--- FATA/dataset/selectedRotateImageFolder.py
+++ FATA/dataset/selectedRotateImageFolder.py
@@ -85,3 +85,154 @@ def rotate_batch(batch, label='rand'):
 		assert isinstance(label, int)
 		labels = torch.zeros((len(batch),), dtype=torch.long) + label
 	return rotate_batch_with_labels(batch, labels), labels
+
+# =========================Rotate ImageFolder Preparations End======================
+
+
+class SelectedRotateImageFolder(datasets.ImageFolder):
+    def __init__(self, root, train_transform,
+                 original=True, rotation=True, rotation_transform=None):
+        super(SelectedRotateImageFolder, self).__init__(root, train_transform)
+        self.original = original
+        self.rotation = rotation
+        self.rotation_transform = rotation_transform
+
+        self.original_samples = self.samples
+
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        img_input = self.loader(path)
+
+        if self.transform is not None:
+            if isinstance(self.transform, list):
+                img = self.transform[1](img_input)
+                img_aug = self.transform[0](img_input)
+            else:
+                img = self.transform(img_input)
+        else:
+            img = img_input
+
+        results = []
+        if self.original:
+            results.append(img)
+            results.append(target)
+            if isinstance(self.transform, list):
+                results.append(img_aug)
+        if self.rotation:
+            if self.rotation_transform is not None:
+                img = self.rotation_transform(img_input)
+            target_ssh = np.random.randint(0, 4, 1)[0]
+            img_ssh = rotate_single_with_label(img, target_ssh)
+            results.append(img_ssh)
+            results.append(target_ssh)
+        return results
+
+    def switch_mode(self, original, rotation):
+        self.original = original
+        self.rotation = rotation
+
+    def set_target_class_dataset(self, target_class_index, logger=None):
+        self.target_class_index = target_class_index
+        self.samples = [(path, idx) for (path, idx)
+                        in self.original_samples if idx in self.target_class_index]
+        self.targets = [s[1] for s in self.samples]
+
+    def set_dataset_size(self, subset_size):
+        num_train = len(self.targets)
+        indices = list(range(num_train))
+        random.shuffle(indices)
+        self.samples = [self.samples[i] for i in indices[:subset_size]]
+        self.targets = [self.targets[i] for i in indices[:subset_size]]
+        return len(self.targets)
+
+    def set_specific_subset(self, indices):
+        self.samples = [self.original_samples[i] for i in indices]
+        self.targets = [s[1] for s in self.samples]
+
+
+def reset_data_sampler(sampler, dset_length, dset):
+    sampler.dataset = dset
+    if dset_length % sampler.num_replicas != 0 and False:
+        sampler.num_samples = math.ceil((dset_length - sampler.num_replicas)
+                                        / sampler.num_replicas)
+    else:
+        sampler.num_samples = math.ceil(dset_length / sampler.num_replicas)
+    sampler.total_size = sampler.num_samples * sampler.num_replicas
+
+
+def prepare_train_dataset(args, use_transforms=True):
+    print('Preparing training data (ori imagenet train)...')
+    tr_transforms_local = tr_transforms if use_transforms else None
+    traindir = os.path.join(args.data, 'train')
+    trset = SelectedRotateImageFolder(traindir, tr_transforms_local,
+                                      original=True, rotation=args.rotation,
+                                        rotation_transform=rotation_tr_transforms)
+    return trset
+
+
+def prepare_train_dataloader(args, trset=None, sampler=None):
+    if sampler is None:
+        trloader = torch.utils.data.DataLoader(trset,
+                                    batch_size=args.train_batch_size,
+                                    shuffle=True,
+                                    num_workers=args.workers, pin_memory=False)
+        train_sampler = None
+    else:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(trset)
+        trloader = torch.utils.data.DataLoader(
+            trset, batch_size=args.batch_size,
+            num_workers=args.workers, pin_memory=True, sampler=train_sampler,
+            drop_last=True)
+    return trloader, train_sampler
+
+
+def prepare_test_data(args, use_transforms=True):	
+    if args.corruption == 'original':
+        te_transforms_local = te_transforms if use_transforms else None
+    elif args.corruption in common_corruptions:
+        te_transforms_local = te_transforms_imageC if use_transforms else None
+    else:
+        assert False, NotImplementedError
+
+    if type(args.data) != str:
+         args.data = args.data.path
+         
+    if not hasattr(args, 'corruption') or args.corruption == 'original':
+        print('Test on the original test set')
+        validdir = os.path.join(args.data, 'val')
+        teset = SelectedRotateImageFolder(validdir, te_transforms_local, original=False,
+                                          rotation=False,
+                                                    rotation_transform=rotation_te_transforms)
+    elif args.corruption in common_corruptions:
+        print('Test on %s level %d' %(args.corruption, args.level))
+        validdir = os.path.join(args.data_corruption, args.corruption, str(args.level))
+        teset = SelectedRotateImageFolder(validdir, te_transforms_local, original=False,
+                                          rotation=False,
+                                                    rotation_transform=rotation_te_transforms)
+    else:
+        raise Exception('Corruption not found!')
+        
+    if not hasattr(args, 'workers'):
+        args.workers = 1
+    teloader = torch.utils.data.DataLoader(teset, batch_size=args.test_batch_size,
+                                           shuffle=args.if_shuffle,
+                                                    num_workers=args.workers, pin_memory=True)
+    return teset, teloader
+
+
+def prepare_test_data_for_train(args, use_transforms=True):
+    te_transforms_local = tr_transforms if use_transforms else None	
+    if args.corruption in common_corruptions:
+        print('Test on %s level %d' %(args.corruption, args.level))
+        validdir = os.path.join(args.data_corruption, args.corruption, str(args.level))
+        teset = SelectedRotateImageFolder(validdir, te_transforms_local, original=False,
+                                          rotation=False,
+                                                    rotation_transform=rotation_te_transforms)
+    else:
+        raise Exception('Corruption not found!')
+        
+    if not hasattr(args, 'workers'):
+        args.workers = 1
+    teloader = torch.utils.data.DataLoader(teset, batch_size=64, shuffle=True,
+                                                    num_workers=args.workers, pin_memory=True)
+    return teset, teloader
